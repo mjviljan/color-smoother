43f5035116a6e7a6b6ffcec1ccb78c5d7a2465db
Mika Viljanen <mika.viljanen@wunderdog.fi>
2021-03-24 18:51:51 +0200

    Add square universe creation

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+pub struct Universe {
+    width: u8,
+    height: u8,
+    cells: Vec<u8>,
+}
+
+impl Universe {
+    pub fn new(cells: Vec<u8>) -> Universe {
+        let root = f32::sqrt(cells.len() as f32);
+
+        if root.fract() != 0.0 {
+            panic!("Can't create a square universe with given cells");
+        }
+
+        // safe because root can't have fractions here
+        unsafe {
+            let side: u8 = root.to_int_unchecked();
+
+            Universe {
+                width: side,
+                height: side,
+                cells,
+            }
+        }
+    }
+
+    pub fn width(&self) -> u8 {
+        self.width
+    }
+
+    pub fn height(&self) -> u8 {
+        self.height
+    }
+
+    pub fn cells(&self) -> &Vec<u8> {
+        &self.cells
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Universe;
+
+    #[test]
+    fn cells_with_power_of_two_length_creates_universe_with_root_of_length_sides() {
+        let expected_side_length: u8 = 5;
+
+        let cells: Vec<u8> = vec![1; expected_side_length.pow(2) as usize];
+        let universe = Universe::new(cells);
+
+        assert_eq!(universe.width, expected_side_length);
+        assert_eq!(universe.height, expected_side_length);
+    }
+}
